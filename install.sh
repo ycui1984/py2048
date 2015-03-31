c71296e7602a2704b8904d33d218200dd00aea8c
Ralph Embree <ralph.embree@brominator.org>
2015-03-31 21:45:44 +0000

    Add instructions for execution to install.sh

--- install.sh
+++ install.sh
@@ -17,4 +17,4 @@ Name=py2048
 Icon=/usr/share/icons/hicolor/64x64/apps/py2048.png
 Categories=Game" | sudo tee /usr/share/applications/py2048.desktop >/dev/null
 echo "Done"
-
+echo "The application can be run from the terminal by typing 'py2048' and can also be accessed from the start menu in the games section."
