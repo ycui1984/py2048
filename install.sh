32e226c46006fddb4463a4dc94629736ede69da8
Ralph Embree <ralph.embree@brominator.org>
2016-01-20 14:53:00 -0500

    Register mime type

--- install.sh
+++ install.sh
@@ -5,6 +5,17 @@ sudo chmod +x /usr/games/py2048
 echo "Downloading icon..."
 sudo wget https://raw.githubusercontent.com/ralphembree/py2048/master/2048.png -O /usr/share/icons/hicolor/64x64/apps/py2048.png
 
+if [ ! -f /usr/share/mime/packages/application-2048.xml ]; then
+    echo "Registering mime type"
+    echo '<?xml version="1.0" encoding="UTF-8"?>
+<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+  <mime-type type="application/2048">
+    <comment>py2048 mime type</comment>
+    <glob pattern="*.2048"/>
+  </mime-type>
+</mime-info>' | sudo tee /usr/share/mime/packages/application-2048.xml
+fi
+
 echo "Adding menu item..."
 echo "[Desktop Entry]
 Version=1.0
@@ -15,6 +26,10 @@ Name[en_US]=py2048
 Exec=py2048
 Name=py2048
 Icon=/usr/share/icons/hicolor/64x64/apps/py2048.png
-Categories=Game" | sudo tee /usr/share/applications/py2048.desktop >/dev/null
+Categories=Game
+MimeType=application/2048" | sudo tee /usr/share/applications/py2048.desktop >/dev/null
+sudo update-mime-database /usr/share/mime
+sudo update-desktop-database /usr/share/applications
 echo -e "\033[1;34mThe application can be run from the terminal by typing 'py2048' and can also be accessed from the start menu in the games section.\033[m"
+
 
